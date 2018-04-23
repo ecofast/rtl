e7cec80485d603b9a2741978469abcf446758789
无尽愿 <adelphicoder@gmail.com>
2018-04-23 18:05:32 +0800

    Create algorithms.go

---
+++ algorithms/algorithms.go
@@ -0,0 +1,19 @@
+package algorithms
+
+import (
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func FisherYatesShuffle(bs []byte) {
+	for i := len(bs) - 1; i >= 1; i-- {
+		j := rand.Intn(i + 1)
+		tmp := bs[i]
+		bs[i] = bs[j]
+		bs[j] = tmp
+	}
+}
